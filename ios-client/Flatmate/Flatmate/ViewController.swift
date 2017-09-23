cdb90618f8f09b5aff448ad2e00a004ceff56708
Patrick Reuter <patrickreuter@outlook.com>
2017-09-23 12:49:13 +0200

    Added some Business stuff

---
+++ ios-client/Flatmate/Flatmate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flatmate
+//
+//  Created by Patrick on 23.09.17.
+//  Copyright © 2017 Team Flatmate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
